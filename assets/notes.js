222cd628833fac475ce15eef55d5ae82b4520761
Harald Billstein <harald.billstein@gmail.com>
2018-02-13 23:19:57 +0100

    Create notes.js

---
+++ assets/notes.js
@@ -0,0 +1,4 @@
+console.log("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+console.log("|H|a|r|a|l|d|B|i|l|l|s|t|e|i|n|");
+console.log("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+console.log("+-+-+-+-+-+-+-+-+ was here  +-+")
