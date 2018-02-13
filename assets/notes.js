e3aee22d563d54ddd147b8002c6c2567cb856c53
Harald Billstein <harald.billstein@gmail.com>
2018-02-13 23:26:46 +0100

    Delete notes.js

--- assets/notes.js
+++
@@ -1,4 +0,0 @@
-console.log("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
-console.log("|H|a|r|a|l|d|B|i|l|l|s|t|e|i|n|");
-console.log("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
-console.log("+-+-+-+-+-+-+-+-+ was here  +-+")
